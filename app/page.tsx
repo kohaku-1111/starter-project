c48a535b63e12a5ef189a699f731c7bf73f13352
kohaku-1111 <133966774+kohaku-1111@users.noreply.github.com>
2025-05-17 11:26:34 +0900

    Add Hero section

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Page() {
+  return (
+    <main>
+      <section className="hero">
+        <h1>法人向けAI研修</h1>
+      </section>
+    </main>
+  );
+}
